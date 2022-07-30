d36f179b8f184b26f3416cca4018b16fe1edd875
paul881023 <e94076021@gs.ncku.edu.tw>
2022-07-30 21:05:24 +0800

    Update pygame_test.py

--- pygame_test.py
+++ pygame_test.py
@@ -7,12 +7,17 @@ FPS = 60
 white = (255,255,255)
 green = (0,255,0)
 red = (255,0,0)
+yellow = (255,255,0)
+black = (0,0,0)
 #大小
 width = 500
 height = 600
 #rock
 rock_width = 20
 rock_height = 20
+#bullet
+bullet_width = 10
+bullet_height = 10   
 #初始化
 pygame.init()
 screen = pygame.display.set_mode((width,height))
@@ -26,8 +31,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((40,50))
         self.image.fill(green)
         self.rect = self.image.get_rect()
-        self.rect.x = 200
-        self.rect.y = 200
+        self.rect.centerx = width / 2
+        self.rect.y = height + 10
 
     def update(self):
         key_pressed = pygame.key.get_pressed()
@@ -42,15 +47,19 @@ class Player(pygame.sprite.Sprite):
 
         print(self.rect.x,",",self.rect.y)   
 
-        if self.rect.x > width:
-            self.rect.x = width
-        if self.rect.x < 0:
-            self.rect.x = 0
+        if self.rect.centerx > width:
+            self.rect.centerx = width
+        if self.rect.centerx < 0:
+            self.rect.centerx = 0
         if self.rect.y > height-50:
             self.rect.y = height-50 
         if self.rect.y < 0:
             self.rect.y = 0
 
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx,self.rect.y)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self) 
@@ -73,13 +82,30 @@ class Rock(pygame.sprite.Sprite):
         if self.rect.left > width + rock_width  or self.rect.right - rock_width < 0:
             self.speedx = -self.speedx
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self) 
+        self.image = pygame.Surface((bullet_width,bullet_height))
+        self.image.fill(yellow)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
 all_sprites = pygame.sprite.Group()
+rocks = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player()       
 all_sprites.add(player)
 for i in range(8):
     rock = Rock()
     all_sprites.add(rock)
-
+    rocks.add(rock)
 running = True
 
 while running:
@@ -88,10 +114,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
      #更新
     all_sprites.update()
+    hits = pygame.sprite.groupcollide(rocks, bullets, True, True)
+    for hit in hits: 
+        rock = Rock()
+        all_sprites.add(rock)
+        rocks.add(rock)
+    hits = pygame.sprite.spritecollide(player,rocks,False,False)
+    if hits:
+        running = False
      #顯示
-    screen.fill(white)
+    screen.fill(black)
     all_sprites.draw(screen)
     pygame.display.update()
     
